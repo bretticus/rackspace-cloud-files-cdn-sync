0653e42278a43ca3ddb8a23b228155df51c8bb73
Brett Millett <bretticus@gmail.com>
2014-08-01 01:30:34 -0600

    basic functionality in place with some bugs to work out

--- lib/rackspace-cdn.php
+++ lib/rackspace-cdn.php
@@ -1,15 +1,24 @@
 <?php
-
 /**
  * Description of rackspace-cdn
  *
  * @author Brett Millett <bmillett@olwm.com>
  */
+
+/**
+ * @property OpenCloud\Rackspace $_client
+ */
 class rackspace_cdn {
 
     static private $_settings = array();
     static private $_client = NULL;
+    static private $_objects = array();
 
+    /**
+     * Main method for performing the sync
+     *
+     * @return boolean
+     */
     static public function doSync() {
 
         $sapi = php_sapi_name();
@@ -17,11 +26,9 @@ class rackspace_cdn {
             echo "This command must be ran from command line!";
             return FALSE;
         }
-        
+
         // autoloader for vendor
         require realpath(dirname(__FILE__) . '/../vendor/autoload.php');
-        
-        
 
         // load settings ini file values if present
         self::$_settings = @parse_ini_file('settings.ini', TRUE);
@@ -32,25 +39,25 @@ class rackspace_cdn {
         self::setSetting('api', 'container', 'Please enter your container name', '/^[a-zA-Z0-9]+$/');
         self::setSetting('api', 'region', 'Please enter your containers region. (ie. DFW,IAD,ORD,LON,' .
                 'HKG,SYD)', '/^(IAD|ORD|DFW|LON|HKG|SYD)$/');
-        
-        self::setSetting('files', 'path', 'Please enter the path to existing uploads folder or equivalent (/path/to/files)', function($path){
+
+        self::setSetting('files', 'path', 'Please enter the path to existing uploads folder or equivalent (/path/to/files)', function($path) {
             return file_exists($path);
         });
-        
+
         self::setSetting('mysql', 'host', 'Please enter database hostname', '/^[a-zA-Z0-9\.\-\_]+$/');
         self::setSetting('mysql', 'host', 'Please enter database name', '/^[a-zA-Z0-9\-\_]+$/');
-        self::setSetting('mysql', 'username', 'Please enter database username (up to 16 ascii chars)', function($username){            
+        self::setSetting('mysql', 'username', 'Please enter database username (up to 16 ascii chars)', function($username) {
             return (ctype_print($username) && strlen($username) <= 16);
         });
-        self::setSetting('mysql', 'username', 'Please enter database password (ascii chars only)', function($password){
+        self::setSetting('mysql', 'username', 'Please enter database password (ascii chars only)', function($password) {
             return ctype_print($password);
         });
-        
+
         // default to US.
         $id_endpoint = OpenCloud\Rackspace::US_IDENTITY_ENDPOINT;
-                
+
         // see if uk endpoint was set.
-        if (isset(self::$_settings['api']['id_endpoint']) && strtolower(trim(self::$_settings['api']['id_endpoint'])) == 'uk' ) {
+        if (isset(self::$_settings['api']['id_endpoint']) && strtolower(trim(self::$_settings['api']['id_endpoint'])) == 'uk') {
             $id_endpoint = OpenCloud\Rackspace::UK_IDENTITY_ENDPOINT;
         }
 
@@ -60,11 +67,70 @@ class rackspace_cdn {
             'apiKey' => self::$_settings['api']['key']
         ));
 
+
+
+        try {
+            // get instance of our object store
+            $objectStoreService = self::$_client->objectStoreService(null, self::$_settings['api']['region']);
+
+            // get instance on our container
+            $container = $objectStoreService->getContainer(self::$_settings['api']['container']);
+
+            // Setup directory iterator for files to consider for upload
+            $files = new RecursiveIteratorIterator(
+                    new RecursiveDirectoryIterator(self::$_settings['files']['path'], RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST
+            );
+
+            // Loop through files and find out if they need uploading
+            foreach ($files as $file) {
+                $real_path = $file->getRealPath();
+                if (!is_dir($real_path)) {
+                    $filename = str_replace(realpath(self::$_settings['files']['path']), '', $real_path);
+                    $dirname = dirname($filename);
+                    if (!isset(self::$_objects[$dirname])) {
+                        // get list of prefix only
+                        $options = array(
+                            'prefix' => $dirname
+                        );
+                        self::$_objects[$dirname] = $container->objectList($options);
+                        foreach (self::$_objects[$dirname] as $object) {
+                            printf("Object name: %s\n", $object->getName());
+                        }
+                    }
+//                    if ($container->objectExists($filename)) {
+//                        $object = $container->getObject($filename);
+//                        printf('File with name: %s exists in object container: %s.' . PHP_EOL, $object->getName(), $container->name);
+//                    } else {
+//                        printf('File with name: %s does not exist in object container: %s.' . PHP_EOL, $filename, $container->name);
+//                    }
+                }
+            }
+        } catch (Guzzle\Http\Exception\ClientErrorResponseException $e) {
+            $status_code = $e->getResponse()->getStatusCode();
+            switch ($status_code) {
+                case 401:
+                    echo 'Unauthorized: Please check your username and API key.' . PHP_EOL;
+                    break;
+                case 404:
+                    echo 'Not Found: Please check your container setting.' . PHP_EOL;
+                    break;
+                default:
+                    echo 'A connection error occurred:' . $e->getResponse()->getReasonPhrase() . PHP_EOL;
+                    break;
+            }
+
+            return FALSE;
+        } catch (UnexpectedValueException $e) {
+            echo 'Please check your file path. Error: ' . $e->getMessage() . PHP_EOL;
+        } catch (Exception $e) {
+            echo 'An error ocurred: ' . $e->getMessage() . PHP_EOL;
+        }
+
         return TRUE;
     }
 
     static private function setSetting($section, $setting, $prompt, $validator) {
-        
+
         // make a preg_match closure otherwise
         if (!(is_object($validator) && ($validator instanceof Closure))) {
             $pattern = $validator;
@@ -74,7 +140,7 @@ class rackspace_cdn {
         }
 
         $get_value = function($value = NULL) use ($prompt, $validator, $setting) {
-            
+
             $attempts = 5; // allow number of attempts before failing.
             $attempts_c = 1; // attempts counter
             // check initial value if supplied.
